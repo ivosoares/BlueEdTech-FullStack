e9fd4a019bd2191025c8d83d83a9fe06405f5296
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-09-29 22:59:15 -0300

    adiciona metodo de cadastro e listagem de vagas

--- aula-08/front-end/index.js
+++ aula-08/front-end/index.js
@@ -1,4 +1,4 @@
-const urlApi = 'http://localhost:3000/vagas';
+const urlApi = 'http://localhost:3000/tarefas';
 const lista = document.getElementById('lista');
 // const getVagas = () => {
 //   const fetchPromisse = fetch(urlApi);
@@ -13,6 +13,8 @@ const lista = document.getElementById('lista');
 //   })
 // }
 
+
+// Faz uma requisicao do tipo [GET] que recebe todas as vagas cadastradas.
 const getVagas = async () => {
   const response = await fetch(urlApi);
   const data = await response.json();
@@ -35,3 +37,53 @@ const getVagas = async () => {
   })
 }
 getVagas();
+
+// [POST] que cadastra as novas vagas no backend.
+const postVaga = async (evento) => {
+  // previne a pagina de atualizar devido ao evento de submit do botao ter sido executado
+  evento.preventDefault();
+
+  // precisamos pegar os valores que o usuario preenche no formulario
+  // buscar o input e buscar o seu value.
+  let titulo = document.getElementById('titulo').value;
+  let descricao = document.getElementById('descricao').value;
+  let salario = document.getElementById('salario').value;
+  let senioridade = document.getElementById('senioridade').value;
+
+  // adicionamos os valores dos inputs em campos do nosso objeto vaga
+  const vaga = {
+    titulo,
+    descricao,
+    salario,
+    senioridade
+  }
+  console.log(vaga);
+
+
+  // estamos configurando a nossa requisicao antes dela ser disparada
+  const request = new Request(urlApi, {
+    method: 'POST',
+    body: JSON.stringify(vaga),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+  
+  // chamamos a funcao fetch de forma assincrona de acordo com as nossas configuracoes anteriores
+  const response = await fetch(request);
+  // pegamos o resultado do fetch assincrono e acessamos o body no formato json
+  const result = await response.json();
+
+  titulo = '';
+  descricao = '';
+  salario = '';
+  senioridade = '';
+
+  lista.innerHTML = '';
+
+  if(result) {
+    getVagas();
+  }
+
+}
+
